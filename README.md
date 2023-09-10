db4974571589368827531676e690fc34da980333
John Christensen <johnawesomejr@gmail.com>
2023-09-10 08:00:24 -0500

    more

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 1. run with docker ```docker-compose up -d```
 1. find out mysql container id ```docker ps```
 1. enter into the mysql docker container ```docker exec -it <CONTAINER ID> bash```
-1. migrate the database ```mysql -u username -p database_name < /host-machine/dump.sql```
+1. migrate the database ```mysql -u root -ptest mydb < /host-machine/dump.sql```
 1. exit out of of the container ```exit```
--- dump/dump.sql
+++ dump/dump.sql
@@ -29,8 +29,18 @@ CREATE TABLE `colors` (
   `text-color` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=483 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=511 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `colors`
+--
+
+LOCK TABLES `colors` WRITE;
+/*!40000 ALTER TABLE `colors` DISABLE KEYS */;
+INSERT INTO `colors` VALUES (497,'reset','reset','reset'),(498,'red','Vacation','white'),(499,'rgb(255, 255, 0)',NULL,'black'),(500,'rgb(74, 133, 232)',NULL,'white'),(501,'rgb(255, 229, 152)',NULL,'black'),(502,'rgb(240, 194, 49)',NULL,'black'),(503,'rgb(0, 255, 255)',NULL,'black'),(504,'#4b85e8',NULL,'white'),(505,'#0000ff',NULL,'white'),(506,'#9901ff',NULL,'white'),(507,'#ff00ff',NULL,'black'),(508,'#c27ca1',NULL,'white'),(509,'#44818e',NULL,'white'),(510,'#6aa84f',NULL,'black');
+/*!40000 ALTER TABLE `colors` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `day-notes`
@@ -49,6 +59,15 @@ CREATE TABLE `day-notes` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `day-notes`
+--
+
+LOCK TABLES `day-notes` WRITE;
+/*!40000 ALTER TABLE `day-notes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `day-notes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `days`
 --
 
@@ -62,8 +81,17 @@ CREATE TABLE `days` (
   `text-color` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7821 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7826 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `days`
+--
+
+LOCK TABLES `days` WRITE;
+/*!40000 ALTER TABLE `days` DISABLE KEYS */;
+/*!40000 ALTER TABLE `days` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -74,4 +102,4 @@ CREATE TABLE `days` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-10 12:06:14
+-- Dump completed on 2023-09-10 12:58:30
